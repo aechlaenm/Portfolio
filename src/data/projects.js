23f56727fb0c3418beb847fbfcaecbd181a385a8
aechlaenm <teeraphatalt@gmail.com>
2025-08-31 14:02:42 +0700

    update2

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,9 @@
+import tankVideo from '@/assets/videos/TankSystem.mp4'
+import heliVideo1 from '@/assets/videos/HeliSystem1.mp4'
+import heliVideo2 from '@/assets/videos/HeliSystem2.mp4'
+import vehicleVideo from '@/assets/videos/vehicleSystem.mp4'
+import pikapikaVideo from '@/assets/videos/PikaPikaSystem.mp4'
+
 export const projects = [
   {
     id: 'tank-sys',
@@ -5,10 +11,7 @@ export const projects = [
     description: 'An advanced tank system featuring mathematically generated dynamic tracks, a fully integrated suspension system, and a streamlined driving mechanism.',
     tags: ['Roblox', 'Physics', 'Vehicles'],
     media: [
-      {
-        type: 'video',
-        src : "https://cdn.discordapp.com/attachments/1152393137034100777/1411233178152337498/TankSystem_2.mp4?ex=68b3e8d1&is=68b29751&hm=82a7cc6d4ea707d5dd39b040dc5cad196b60df035e22df3bf700bdbc5aa559c2&"
-      }
+      { type: 'video', src: tankVideo }
     ]
   },
   {
@@ -17,14 +20,8 @@ export const projects = [
     description: 'A helicopter system built in Roblox using body movers and world gravity, featuring an integrated weapon system and flight control system.',
     tags: ['Roblox', 'Physics', 'Vehicles'],
     media: [
-      {
-        type: 'video',
-        src: "https://cdn.discordapp.com/attachments/1358092627060330669/1358871012929831052/2025-04-08_01-19-06.mp4?ex=68b49440&is=68b342c0&hm=7d5a78732fba65c5956b639f1a7d4b24e34f2813cd098ebcd22d628dd1aaaa47&"
-      },
-      {
-        type: 'video',
-        src: "https://cdn.discordapp.com/attachments/1358092627060330669/1385252206521094255/Roblox-2025-06-19T13_35_19.941Z.mp4?ex=68b4f8e6&is=68b3a766&hm=fc68055fa7f28b4f7d8344e46e512bcde2186dd907d76bb153d97900252eea17&"
-      }
+      { type: 'video', src: heliVideo1 },
+      { type: 'video', src: heliVideo2 }
     ]
   },
   {
@@ -33,10 +30,7 @@ export const projects = [
     description: 'A vehicle system built with simple CylinderConstraints, featuring suspension, a basic driving system, and an automatic flip mechanism.',
     tags: ['Roblox', 'Physics', 'Vehicles'],
     media: [
-      {
-        type: 'video',
-        src: "https://cdn.discordapp.com/attachments/1152393137034100777/1411243502716588125/Roblox-2025-08-30T06_46_19.240Z_2_1_1.mp4?ex=68b3f26e&is=68b2a0ee&hm=5e802f60b093c64bd7649e865aad93e63f0d2c21badf477f560db2bdc5f91650&"
-      }
+      { type: 'video', src: vehicleVideo }
     ]
   },
     {
@@ -45,10 +39,7 @@ export const projects = [
     description: 'The Pika Pika Fruit currently offers three skills. The first is Light Kick, which launches a light-infused projectile forward to damage humanoids. The second is Light Jewel, a rapid-fire technique that spreads multiple light projectiles forward. The third is Light Flight, allowing the user to transform into light and travel freely across any distance.',
     tags: ['Roblox', 'Skill', 'Combat'],
     media: [
-          {
-        type: 'video',
-        src: 'https://cdn.discordapp.com/attachments/1152393137034100777/1411245420352503918/ssvid.net--Light-Fruit_v720P_1.mp4?ex=68b3f437&is=68b2a2b7&hm=97677e14113f7cb5ef12e8c63ea4ec944e22db0f95070c1c757a24b47605723a&'
-      }
+      { type: 'video', src: pikapikaVideo }
     ]
   },
 ]
